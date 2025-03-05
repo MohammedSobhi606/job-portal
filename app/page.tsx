04f1283ba9c2eb68c938697f12b97e69cb484fc3
MohammedSobhi606 <Mohammmedsobhi606>
2025-03-05 17:56:05 +0200

    add nav an footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobResults from "@/components/JobResults";
 import H1 from "@/components/ui/h1";
-import { JobFilterValues } from "@/lib/validation";
+import { jobFilterSchema, JobFilterValues } from "@/lib/validation";
+import { Metadata } from "next";
 
 // interface for searchParams
 interface IPageProps {
@@ -13,11 +14,37 @@ interface IPageProps {
     remote?: string; // as any search parameters is string
   };
 }
+// metadata
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+    ? `${type} developer jobs`
+    : remote
+    ? "Remote developer jobs"
+    : "All developer jobs";
 
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+}
+
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: IPageProps): Metadata {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | portal jobs`,
+  };
+}
 export default async function Home({
   searchParams: { q, location, type, remote },
 }: IPageProps) {
-  const filterValues: JobFilterValues = {
+  const filterValues: JobFilterValues = await {
     q,
     location,
     type,
@@ -26,7 +53,7 @@ export default async function Home({
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>Developer jobs</H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
       <section className="flex-col flex md:flex-row gap-4">
